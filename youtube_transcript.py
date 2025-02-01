f4018327020829b56a99583a0542463c1ac5e6fa
waynetboote <waynetboote@gmail.com>
2025-02-01 11:41:33 +0000

    Update youtube_transcript.py

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -2,5 +2,14 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 def get_transcript(youtube_url):
     video_id = youtube_url.split("v=")[1].split("&")[0]
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+
+def get_transcript(youtube_url):
+    video_id = youtube_url.split("v=")[1].split("&")[0]
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        return " ".join([entry["text"] for entry in transcript])
+    except TranscriptsDisabled:
+        return "Error: This video does not have subtitles available."
+
     return " ".join([entry["text"] for entry in transcript])
